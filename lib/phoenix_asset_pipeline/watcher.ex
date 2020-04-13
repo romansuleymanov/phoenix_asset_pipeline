1f35fcc9f3e9ec8d7d8d9a71ad9b824a498f19e4
Yuri S <fudoshiki.ari@gmail.com>
2020-04-13 22:51:05 +0500

    Use pattern matching

--- lib/phoenix_asset_pipeline/watcher.ex
+++ lib/phoenix_asset_pipeline/watcher.ex
@@ -19,22 +19,22 @@ defmodule PhoenixAssetPipeline.Watcher do
   def handle_info({:file_event, _, {path, _}}, %{watcher_pid: _} = state) do
     extname = Path.extname(path)
     [base_path | key_list] = metadata(extname)
-
     %{"path" => path} = Regex.named_captures(~r/\/#{base_path}\/(?<path>.+)#{extname}/, path)
     key = asset_key(extname, path)
 
-    if key in key_list do
-      delete(key)
-    else
-      for key <- key_list, do: delete(key)
-    end
-
+    clean_storage(key, key_list)
     {:noreply, state}
   end
 
   defp asset_key(".sass", path), do: Sass.asset_key(path)
   defp asset_key(".coffee", path), do: CoffeeScript.asset_key(path)
   defp asset_key(_, path), do: path
+
+  defp clean_storage(key, keys) when key in keys, do: delete(key)
+
+  defp clean_storage(key, keys) do
+    for key <- key_list, do: delete(key)
+  end
 
   defp metadata(".sass"), do: [Sass.base_path(), Sass.key_list()]
   defp metadata(".coffee"), do: [Sass.base_path(), CoffeeScript.key_list()]
