6ae304999f674f7e237f6946113e71693abcb1ba
Yuri S <fudoshiki.ari@gmail.com>
2020-04-17 16:33:29 +0500

    Use alphabetic order, update plug_cowboy

--- lib/phoenix_asset_pipeline/endpoint.ex
+++ lib/phoenix_asset_pipeline/endpoint.ex
@@ -6,10 +6,22 @@ defmodule PhoenixAssetPipeline.Endpoint do
   alias Plug.SSL
   alias PhoenixAssetPipeline.Plugs.{JavaScript, Static}
 
-  plug :ssl
-  plug :javascript
-  plug :static
-  plug :not_found
+  plug(:ssl)
+  plug(:static)
+  plug(:javascript)
+  plug(:not_found)
+
+  def __handler__(conn, opts) do
+    {:plug, conn, __MODULE__, opts}
+  end
+
+  defp javascript(conn, _opts) do
+    JavaScript.call(conn, [])
+  end
+
+  defp not_found(conn, _opts) do
+    send_resp(conn, 404, "not found")
+  end
 
   defp ssl(conn, opts) do
     force_ssl = Keyword.get(opts, :force_ssl, false)
@@ -20,18 +32,7 @@ defmodule PhoenixAssetPipeline.Endpoint do
     end
   end
 
-  defp javascript(conn, _opts) do
-    JavaScript.call(conn, [])
-  end
-
   defp static(conn, _opts) do
     Static.call(conn, at: "/img", from: "assets/images")
   end
-
-  defp not_found(conn, _opts) do
-    send_resp(conn, 404, "not found")
-  end
-
-  def __handler__(conn, opts),
-    do: {:plug, conn, __MODULE__, opts}
 end
